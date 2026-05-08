8fe250b5be272e2d283bfa4592bf89b8e7923da4
Alok Raj <alokr8950@gmail.com>
2026-05-08 14:06:42 +0530

    fixed api url

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://mern-auth-master.onrender.com',
+  credentials: 'include',
+});
 
 export const apiSlice = createApi({
   baseQuery,
