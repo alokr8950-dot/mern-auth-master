e08f86e9ca509936a72815854a7ba25a3d271b5d
Alok Raj <alokr8950@gmail.com>
2026-05-08 15:24:34 +0530

    fixed frontend env

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://mern-auth-master.onrender.com',
+  baseUrl: import.meta.env.VITE_API_URL,
   credentials: 'include',
 });
 
