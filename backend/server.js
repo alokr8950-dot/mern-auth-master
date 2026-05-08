1d3062ba0526a1b19df5ff83ea48b2b9ca3449a5
Alok Raj <alokr8950@gmail.com>
2026-05-08 13:46:22 +0530

    fixed backend deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 
@@ -34,22 +33,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
 
-// Production
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, 'frontend', 'dist', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+// Home Route
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
 
 
 // Error Middleware
